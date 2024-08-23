781b95eddad209483ff71bedd09ad1f275ce5986
anand <anandanand99599@gmail.com>
2024-08-23 21:41:31 +0530

    kubernetes

---
+++ run.sh
@@ -0,0 +1,17 @@
+source /data/secrets
+git clone https://github.com/raghudevopsb78/${PROJECT_NAME}-${COMPONENT}
+cd ${PROJECT_NAME}-$COMPONENT
+
+if [ "${db_type}" == 'mongo' ]; then
+  curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+  mongo --tls --host $DB_HOST:27017 --tlsCAFile global-bundle.pem --username ${DB_USER} --password $DB_PASS <schema/${COMPONENT}.js
+fi
+
+if [ "${db_type}" == 'mysql' ]; then
+  echo 'show databases;' | mysql -h${DB_HOST} -u${RDS_USER} -p${RDS_PASS} | grep cities
+  if [ $? -eq 0 ]; then exit ; fi
+  mysql -h${DB_HOST} -u${RDS_USER} -p${RDS_PASS} <schema/${COMPONENT}.sql
+fi
+
+
+
